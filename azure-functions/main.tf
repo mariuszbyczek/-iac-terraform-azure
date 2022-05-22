795113e76891482a49d4cb20bc46879c7bbcb9a5
mariuszbyczek <91824400+mariuszbyczek@users.noreply.github.com>
2022-05-22 10:19:15 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "this" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "this" {
   name                     = "linuxfunctionappsa"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "example" {
+resource "azurerm_service_plan" "this" {
   name                = "example-app-service-plan"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "example" {
   sku_name            = "Y1"
 }
 
-resource "azurerm_linux_function_app" "example" {
+resource "azurerm_linux_function_app" "this" {
   name                = "example-linux-function-app"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -30,7 +30,7 @@ resource "azurerm_linux_function_app" "example" {
   site_config {}
 }
 
-data "azurerm_function_app_function" "example" {
-  id                =  local.id
+data "azurerm_function_app_function" "this" {
+  id                =  "name 123"
   resource_group_name = azurerm_resource_group.example.name
 }
