8f48b23e9778ef126fdc2a39c5891492872d3304
mariuszbyczek <91824400+mariuszbyczek@users.noreply.github.com>
2022-05-22 11:20:10 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,27 +1,26 @@
 resource "azurerm_resource_group" "this" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var.name_azurerm_resource_group
+  location = var.location_azurerm_resource_group
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                     = var.name_azurerm_storage_account
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = var.tier_azurerm_storage_account
+  account_replication_type = var.replication_type_azurerm_storage_account
 }
 
 resource "azurerm_storage_container" "this" {
-  name                  = "content"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+  name                  = var.name_azurerm_storage_container
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = var.access_type_azurerm_storage_container
 }
 
 resource "azurerm_storage_blob" "this" {
-  url                    = "www.niewiem.pl"
-  name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
-  type                   = "Block"
-  source                 = "some-local-file.zip"
-}
+  url                    = var.url_azurerm_storage_blob
+  name                   = var.name_azurerm_storage_blob
+  storage_account_name   = azurerm_storage_account.this.name
+  storage_container_name = azurerm_storage_container.this.name
+  type                   = var.type_azurerm_storage_blob
+  source                 = var.source_azurerm_storage_blob
